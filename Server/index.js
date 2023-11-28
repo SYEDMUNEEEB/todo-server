1c86b57ac0d3235c4234576192f0d5e22641a707
https://github.com/SYEDMUNEEEB <muneebsyed70@gmail.com>
2023-11-28 14:13:46 +0500

    Your commit message

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose= require ('mongoose')
+const TodoItemRoute = require('./routes/todoItems');
+const dotenv=require('dotenv').config()
+const cors=require('cors')
+const app=express()
+app.use(express.json())
+const PORT = process.env.PORT || 5500;
+
+//use cors
+app.use(cors());
+//connect to mongodb ..
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connected"))
+  .catch(err => console.error("Error connecting to database:", err));
+
+
+  app.use('/', TodoItemRoute);
+//connect to server
+app.listen(PORT, ()=> console.log("Server connected") );
